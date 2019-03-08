9b0dca2d985b97f9427d8df14ac71b650a659fb2
Gguigre <gui.gremi@gmail.com>
2019-03-08 19:11:11 +0100

    Updates

---
+++ precache-manifest.9d3cae5ec8c2054029ac9bea8d0e88af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "99354d081a52c4de0478f95e317f148f",
+    "url": "/yatilpot/static/media/logo.99354d08.svg"
+  },
+  {
+    "revision": "92c6c0edb0510a0e916b",
+    "url": "/yatilpot/static/js/runtime~main.83dd5c2a.js"
+  },
+  {
+    "revision": "7ba01df29d67c9a7ee5e",
+    "url": "/yatilpot/static/js/main.689133a9.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/yatilpot/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "7ba01df29d67c9a7ee5e",
+    "url": "/yatilpot/static/css/main.53bb2071.chunk.css"
+  },
+  {
+    "revision": "aa8161155a23a302e1a416fbf54a1899",
+    "url": "/yatilpot/index.html"
+  }
+];
