50bf9e6e9e27fd116d060a5201d0cb309e04f40f
Gguigre <gui.gremi@gmail.com>
2020-03-27 18:49:42 +0100

    add telepot redirect

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import virus from "./assets/virus.png";
 
 class App extends Component {
   componentDidMount() {
-    if (getAnswer() === ANSWER_YOU_ARE_LATE) {
-      window.location.replace(
-        "https://www.google.com/maps/place/C'D%C3%A9Cal%C3%A9/@48.8844574,2.3174386,17z/data=!4m12!1m6!3m5!1s0x47e66fb23363c55b:0xe401d4ced0cb52b9!2zQydEw6lDYWzDqQ!8m2!3d48.8844574!4d2.3196273!3m4!1s0x47e66fb23363c55b:0xe401d4ced0cb52b9!8m2!3d48.8844574!4d2.3196273"
-      );
-    }
+    // if (getAnswer() === ANSWER_YOU_ARE_LATE) {
+    //   window.location.replace(
+    //     "https://www.google.com/maps/place/C'D%C3%A9Cal%C3%A9/@48.8844574,2.3174386,17z/data=!4m12!1m6!3m5!1s0x47e66fb23363c55b:0xe401d4ced0cb52b9!2zQydEw6lDYWzDqQ!8m2!3d48.8844574!4d2.3196273!3m4!1s0x47e66fb23363c55b:0xe401d4ced0cb52b9!8m2!3d48.8844574!4d2.3196273"
+    //   );
+    // }
+
+    setTimeout(() => {
+      window.location.replace("https://discord.gg/hAGA8Fp");
+    }, 1000);
   }
 
   render() {
