f36674d9232384d576fa4c6e7bcdd5e2e5b54dc4
Gguigre <gui.gremi@gmail.com>
2021-05-20 15:33:57 +0200

    feat: remove lockdown mode

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import { getAnswer, ANSWER_YOU_ARE_LATE } from "./lib/getAnswer";
 import { AnswerPicto, AnswerText } from "./components";
 import virus from "./assets/virus.png";
 
+const LOCKDOWN = false;
 class App extends Component {
   componentDidMount() {
-    // if (getAnswer() === ANSWER_YOU_ARE_LATE) {
-    //   window.location.replace(
-    //     "https://www.google.com/maps/place/C'D%C3%A9Cal%C3%A9/@48.8844574,2.3174386,17z/data=!4m12!1m6!3m5!1s0x47e66fb23363c55b:0xe401d4ced0cb52b9!2zQydEw6lDYWzDqQ!8m2!3d48.8844574!4d2.3196273!3m4!1s0x47e66fb23363c55b:0xe401d4ced0cb52b9!8m2!3d48.8844574!4d2.3196273"
-    //   );
-    // }
-
-    setTimeout(() => {
-      window.location.replace("https://discord.gg/hAGA8Fp");
-    }, 1000);
+    if (LOCKDOWN) {
+      // setTimeout(() => {
+      //   window.location.replace("https://discord.gg/hAGA8Fp");
+      // }, 1000);
+    } else {
+      if (getAnswer() === ANSWER_YOU_ARE_LATE) {
+        window.location.replace(
+          "https://www.google.com/maps/place/C'D%C3%A9Cal%C3%A9/@48.8844574,2.3174386,17z/data=!4m12!1m6!3m5!1s0x47e66fb23363c55b:0xe401d4ced0cb52b9!2zQydEw6lDYWzDqQ!8m2!3d48.8844574!4d2.3196273!3m4!1s0x47e66fb23363c55b:0xe401d4ced0cb52b9!8m2!3d48.8844574!4d2.3196273"
+        );
+      }
+    }
   }
 
   render() {
@@ -23,10 +26,12 @@ class App extends Component {
         <header className="App-header">
           <AnswerPicto />
           <AnswerText />
-          <img
-            src={virus}
-            style={{ position: "absolute", width: "70%", opacity: 0.95 }}
-          />
+          {LOCKDOWN && (
+            <img
+              src={virus}
+              style={{ position: "absolute", width: "70%", opacity: 0.95 }}
+            />
+          )}
         </header>
       </div>
     );
